cd5cd13c005d19a5ab0b17fe12456cfbce1eb0e0
MELR22 <roel.melman@gmail.com>
2026-08-14 11:00:28 +0200

    new url

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // NEVER put a Supabase service_role key here.
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://oehndpyzeoywfypqtsjs.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://oehndpyzeoywfypqtsjs.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9laG5kcHl6ZW95d2Z5cHF0c2pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2ODk1MTAsImV4cCI6MjEwMjI2NTUxMH0.ibV8ATmFR1MSmxe3uQGlIKlJh_LcTxpS22sr7HNbTEE"
 };
